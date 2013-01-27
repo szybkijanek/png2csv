4e52ab0a517bdd7ebe61702761a88bb869698f49
Paweł Ksieniewicz <pawel@ksienie.com>
2013-01-27 12:19:46 +0100

    Spłaszczenie projektu

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  png2csv
+//
+//  Created by Paweł Ksieniewicz on 19.01.2013.
+//  Copyright (c) 2013 Paweł Ksieniewicz. All rights reserved.
+//
+
+#include "structures.h"
+#include "analizer.h"
+
+#include "patto.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    /*
+    patto *patogen = new patto;
+    patogen -> load_file("-.png");
+    printf("[%lli]\n", patogen -> full_pattern());
+    */
+    analizer *meteorogram_analizer = new analizer;
+
+    meteorogram_analizer -> load_file((char *)argv[1]);
+    
+    for (int i=2; i<=24; i+=2) meteorogram_analizer -> println_for_hour_interval(i);
+    
+    return 0;
+}
+
---
+++ pngmatrix.h
@@ -0,0 +1,43 @@
+//
+//  pngmatrix.h
+//  png2csv
+//
+//  Created by Paweł Ksieniewicz on 19.01.2013.
+//  Copyright (c) 2013 Paweł Ksieniewicz. All rights reserved.
+//
+
+#ifndef __png2csv__pngmatrix__
+#define __png2csv__pngmatrix__
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define PNG_DEBUG 3
+#include <UnixImageIO/png.h>
+
+class pngmatrix {
+    int x, y;
+    
+    
+    
+    png_structp png_ptr;
+    png_infop info_ptr;
+    int number_of_passes;
+    png_bytep * row_pointers;
+    
+public:
+    png_byte bit_depth;
+    png_byte color_type;
+
+    int width, height;
+    
+    void read_png_file(char* file_name);
+    void process_file(void);
+    int red_pixel(int _x, int _y);
+    int green_pixel(int _x, int _y);
+};
+
+#endif /* defined(__png2csv__pngmatrix__) */
